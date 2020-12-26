61dfcd4f9eac8344bd501c5ed4ef1844e899d00c
zwartapps <zwart.apps@gmail.com>
2020-12-26 15:58:36 +0000

    tarea1

---
+++ prueba.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Prueba</title>
+</head>
+
+<body>
+
+    <?php
+    echo "<h2> Hola, mundo desde fuera <h2>";
+    ?>
+
+    <h3>Pula aqui para ver el archivo <a href="https://phpcursophp.000webhostapp.com/phpinfo.php">phpinfo.php</a></h3>
+
+
+</body>
+
+</html>
